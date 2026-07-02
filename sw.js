65af82c2e77d4f01795edd6fced3dfaadc21c7ec
AbsconBishop <ns.sipciak@gmail.com>
2026-07-02 23:10:00 +0200

    complete visual overhaul

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='selected-transport-v1';
+const CACHE='selected-transport-v2';
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(self.clients.claim()); });
 self.addEventListener('fetch', e => {
